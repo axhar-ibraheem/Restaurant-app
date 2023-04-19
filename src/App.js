81536702c1fac06cda49dde82adceb64985c40dc
axhar-ibraheem <azharibraheem482@gmail.com>
2023-04-19 16:56:21 +0530

    restaurant summary added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Layout/Header";
 import React from "react";
+import RestuarantSummary from "./components/Layout/RestuarantSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
+      <main>
+        <RestuarantSummary></RestuarantSummary>
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/components/Layout/RestuarantSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./RestaurantSummary.css";
+
+const RestuarantSummary = () => {
+  return (
+    <div className="restaurant-summary">
+      <h1>delicious food, delivered to you</h1>
+      <p>
+        Choose your favourite meal from our broad selection of available meals
+        and enjoy a delecious lunch or dinner at your home.
+      </p>
+      <p>
+        All our meals are cooked with high quality ingredients, just in-time and
+        of course by experienced chefs!
+      </p>
+    </div>
+  );
+};
+
+export default RestuarantSummary;
